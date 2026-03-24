5b91560ac442166bce0c4cac0d915640520f161f
nguyentrinhvo <nguyentrinhvo01012005@gmail.com>
2026-03-24 12:19:16 +0700

    setup fe

---
+++ FE/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import DashboardLayout from './components/layout/DashboardLayout';
+import Home from './pages/dashboard/Home';
+// Import các trang khác tương tự...
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected Dashboard Routes */}
+        <Route path="/" element={<DashboardLayout />}>
+          <Route index element={<Home />} />
+          <Route path="accounts" element={<div>Account Management Page</div>} />
+          <Route path="goals" element={<div>Goals Page</div>} />
+          <Route path="reports" element={<div>Report Page</div>} />
+          <Route path="transactions" element={<div>Transactions Page</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
